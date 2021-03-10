3d7991cd015aa9f2995dd86ab22bee8cfcfa8cc7
marodi-mykhailo <mykhailo.marodi@gmail.com>
2021-03-10 09:53:55 +0100

    small changes, add cart, add single product view, add subscribe section

---
+++ src/components/v-product-item.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="v-product-item">
+        <img class="v-product-item__img" :src="itemData.image || require('@/assets/images/jeans1.jpg')" alt="product-item"/>
+        <div class="v-product-item__content">
+            <div class="v-product-item__content-title">title</div>
+            <div class="v-product-item__content-price">Price</div>
+            <p class="v-product-item__content-description">Description</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss">
+
+</style>
